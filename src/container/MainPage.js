5a54a3c81c0a3ba12c2b5080ab20f37f58d5594c
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-18 14:38:17 +0100

    adds Modal

--- src/container/MainPage.js
+++ src/container/MainPage.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 import Basket from '../components/Basket';
 import ProductsList from '../components/ProductsList';
 
 const MainPage = () => {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState('User');
   const [basketItems, setBasketItems] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   useEffect(() => {
     getProducts();
@@ -28,13 +31,41 @@ const MainPage = () => {
     setBasketItems(copyArray);
   };
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <>
       <h1>E-shop</h1>
+      <Modal
+        isOpen={isModalOpen}
+        ariaHideApp={false}
+        contentLabel='User options'
+      >
+        <label htmlFor='name'>Your name: </label>
+        <input
+          type='text'
+          name='name'
+          placeholder={name}
+          onChange={handleNameChange}
+        />
+        <br></br>
+        <label htmlFor='name'>Password: </label>
+        <input type='password' min='1' name='password' />
+        <br></br>
+        <button onClick={toggleModal}>OK</button>
+      </Modal>
+      <p>Welcome {name}!!</p>
       <ProductsList
         products={products}
         onAddToBasketClick={onAddToBasketClick}
       />
+
       <Basket basketItems={basketItems} />
     </>
   );
